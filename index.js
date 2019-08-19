00b3663c39bf9507850b4878836e919666e9acf5
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-19 12:05:36 -0400

    solve the cipher

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-app.use(morgan("dev"));
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-app.get("/sum", (req, res) => {
+app.get('/sum', (req, res) => {
   const { a, b } = req.query;
   if (!a || !b) {
-    return res.status(404).send("must provide both values");
+    return res.status(404).send('must provide both values');
   }
   const c = parseFloat(a) + parseFloat(b);
   res.send(`The sum of ${a} and ${b} is ${c}`);
 });
 
-app.listen(3000, () => console.log("connected on port 3000"));
+app.get('/cipher', (req, res) => {
+  const { text, shift } = req.query;
+  if (!text || !shift) {
+    return res.status(404).send('Must provide both queries.');
+  }
+  const shiftInt = parseInt(shift);
+  const charArr = text.split('');
+  const final = charArr.map(char => {
+    const charNum = char.toUpperCase().charCodeAt(0);
+    if (charNum + shiftInt > 90) {
+      const diff = charNum + shiftInt - 90;
+      return String.fromCharCode(65 + diff - 1);
+    } else {
+      return String.fromCharCode(charNum + shiftInt);
+    }
+  });
+  res.send(`The shifted text is: ${final.join('')}.`);
+});
+
+app.listen(3000, () => console.log('connected on port 3000'));
